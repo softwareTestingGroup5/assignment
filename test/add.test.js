3b62cdbeb039ef17acadb40b69bdb93cefa0df13
Petteri Niemi <beteniemi@gmail.com>
2021-11-30 17:34:18 +0200

    Separate add tests under own tests

--- test/add.test.js
+++ test/add.test.js
@@ -1,10 +1,25 @@
 import add from '../src/add.js';
 
-test('Add', async () => {
+test('Positive integers', async () => {
     expect(add(2, 3)).toBe(5);
+})
+
+test('Negative and positive', async () => {
     expect(add(-1, 1)).toBe(0);
+})
+
+test('Zeros', async () => {
     expect(add(0, 0)).toBe(0);
+})
+
+test('Both negative', async () => {
     expect(add(-1, -1)).toBe(-2);
+})
+
+test('Larger integers', async () => {
     expect(add(256, -512)).toBe(-256);
+})
+
+test('Floats', async () => {
     expect(add(1.25, 1.25)).toBe(2.5);
 })
