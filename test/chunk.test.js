cfde3bf1f97a8ac95d0c6dbadb3a145d6e5e7b9c
Petteri Niemi <beteniemi@gmail.com>
2021-11-30 19:19:16 +0200

    Add more tests for chunk.js

--- test/chunk.test.js
+++ test/chunk.test.js
@@ -5,17 +5,29 @@ var arrayOneElem = ['one'];
 var emptyArray = [];
 
 test('Chunk size one', async () => {
-    expect(chunk(array, 1)).toBe([[1], [2], [3], [5]]);
+    expect(chunk(array, 1)).toStrictEqual([[1], [2], [3], [5]]);
 })
+
 test('Chunk size two', async () => {
-    expect(chunk(array, 2)).toBe([[1, 2], [3, 4], [5]]);
+    expect(chunk(array, 2)).toStrictEqual([[1, 2], [3, 4], [5]]);
 })
+
 test('Chunk size greater than array size', async () => {
-    expect(chunk(array, 7)).toBe([[1, 2, 3, 4, 5]]);
+    expect(chunk(array, 7)).toStrictEqual([[1, 2, 3, 4, 5]]);
 })
+
 test('Empty array', async () => {
-    expect(chunk(emptyArray, 2)).toBe([]);
+    expect(chunk(emptyArray, 2)).toStrictEqual([]);
 })
+
 test('Array of one element', async () => {
-    expect(chunk(arrayOneElem, 2)).toBe([['one']]);
+    expect(chunk(arrayOneElem, 2)).toStrictEqual([['one']]);
+})
+
+test('Chunk size zero', async () => {
+    expect(chunk(array, 0)).toStrictEqual([]);
+})
+
+test('Null', async () => {
+    expect(chunk(null, 1)).toStrictEqual([]);
 })
