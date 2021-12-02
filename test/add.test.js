e68c63e9fe348b9cd9ace9fa281ab42455e2fcbc
Petteri Niemi <beteniemi@gmail.com>
2021-12-02 14:03:37 +0200

    Change add test matchers to toStrictEqual

--- test/add.test.js
+++ test/add.test.js
@@ -1,25 +1,25 @@
 import add from '../src/add.js';
 
 test('Positive integers', async () => {
-    expect(add(2, 3)).toBe(5);
+    expect(add(2, 3)).toStrictEqual(5);
 })
 
 test('Negative and positive', async () => {
-    expect(add(-1, 1)).toBe(0);
+    expect(add(-1, 1)).toStrictEqual(0);
 })
 
 test('Zeros', async () => {
-    expect(add(0, 0)).toBe(0);
+    expect(add(0, 0)).toStrictEqual(0);
 })
 
 test('Both negative', async () => {
-    expect(add(-1, -1)).toBe(-2);
+    expect(add(-1, -1)).toStrictEqual(-2);
 })
 
 test('Larger integers', async () => {
-    expect(add(256, -512)).toBe(-256);
+    expect(add(256, -512)).toStrictEqual(-256);
 })
 
 test('Floats', async () => {
-    expect(add(1.25, 1.25)).toBe(2.5);
+    expect(add(1.25, 1.25)).toStrictEqual(2.5);
 })
